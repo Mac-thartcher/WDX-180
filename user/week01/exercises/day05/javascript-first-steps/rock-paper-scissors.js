e96b9d63d6cb65e71410f76dd6b2661ecd77a989
Mac-thartcher <162346125+Mac-thartcher@users.noreply.github.com>
2025-10-15 08:44:21 +0300

    javascript-rock-paper-scissors

--- user/week01/exercises/day05/javascript-first-steps/rock-paper-scissors.js
+++ user/week01/exercises/day05/javascript-first-steps/rock-paper-scissors.js
@@ -1,15 +1,21 @@
-function getRandomMove(){
+// Function that returns a random move, e.g. rock, paper, scissors:
+function getRandomMove() {
   const moves = ["rock", "paper", "scissors"];
   return moves[Math.floor(Math.random() * 3)];
 }
 
-function checkMove(move, computerMove){
+// Given a move, (e.g. rock, paper, scissors) and a random computer move. Returns the result which must be strictly one of the following: "won", "lost", "draw"
+function checkMove(move, computerMove) {
   const validMoves = ["rock", "paper", "scissors"];
   move = move.trim().toLowerCase();
 
-  if (!validMoves.includes(move)) return "invalid move";
+  if (!validMoves.includes(move)) {
+    return "invalid move";
+  }
 
-  if (move === computerMove) return "draw";
+  if (move === computerMove) {
+    return "draw";
+  }
 
   if (
     (move === "rock" && computerMove === "scissors") ||
@@ -21,31 +27,55 @@ function checkMove(move, computerMove){
     return "lost";
   }
 }
-
+// Don't worry about the code below. It's just there to test your code above.
 const drawn = new Set();
-for ( let i = 0; i < 100; i++ ){
+for (let i = 0; i < 100; i++) {
   const randomMove = getRandomMove();
   drawn.add(randomMove);
-  const oneOfTheThreeMoves = randomMove === 'rock' || randomMove === 'paper' || randomMove === 'scissors';
-  if ( !oneOfTheThreeMoves ){
-    throw new Error("Ops! Expected rock, paper or scissors, instead got " + randomMove);
+  const oneOfTheThreeMoves =
+    randomMove === "rock" ||
+    randomMove === "paper" ||
+    randomMove === "scissors";
+  if (!oneOfTheThreeMoves) {
+    throw new Error(
+      "Ops! Expected rock, paper or scissors, instead got " + randomMove
+    );
   }
 }
 
-if ( !drawn.has("rock") || !drawn.has("paper") || !drawn.has("scissors") ){
+if (!drawn.has("rock") || !drawn.has("paper") || !drawn.has("scissors")) {
   throw new Error("Ops! Did not find all three moves in the results!");
 }
 
-console.log("Game Result 1: ", checkMove("rock", "rock"));       // draw
-console.log("Game Result 2: ", checkMove("rock", "paper"));      // lost
-console.log("Game Result 3: ", checkMove("rock", "scissors"));   // won
-console.log("Game Result 4: ", checkMove("paper", "paper"));     // draw
-console.log("Game Result 5: ", checkMove("paper", "scissors"));  // lost
-console.log("Game Result 6: ", checkMove("paper", "rock"));      // won
-console.log("Game Result 7: ", checkMove("scissors", "scissors"));// draw
-console.log("Game Result 8: ", checkMove("scissors", "rock"));   // lost
-console.log("Game Result 9: ", checkMove("scissors", "paper"));  // won
-console.log("Game Result 10: ", checkMove("pencil", "rock"));    // invalid move
+const gameResult1 = checkMove("rock", "rock");
+console.log("Game Result 1: ", gameResult1); // Should be draw
+
+const gameResult2 = checkMove("rock", "paper");
+console.log("Game Result 2: ", gameResult2); // Should be lost
+
+const gameResult3 = checkMove("rock", "scissors");
+console.log("Game Result 3: ", gameResult3); // Should be won
+
+const gameResult4 = checkMove("paper", "paper");
+console.log("Game Result 4: ", gameResult4); // Should be draw
+
+const gameResult5 = checkMove("paper", "scissors");
+console.log("Game Result 5: ", gameResult5); // Should be lost
+
+const gameResult6 = checkMove("paper", "rock");
+console.log("Game Result 6: ", gameResult6); // Should be won
+
+const gameResult7 = checkMove("scissors", "scissors");
+console.log("Game Result 7: ", gameResult7); // Should be draw
+
+const gameResult8 = checkMove("scissors", "rock");
+console.log("Game Result 8: ", gameResult8); // Should be lost
+
+const gameResult9 = checkMove("scissors", "paper");
+console.log("Game Result 9: ", gameResult9); // Should be won
+
+const gameResult10 = checkMove("pencil", "rock");
+console.log("Game Result 10: ", gameResult10); // Should be invalid move
 
 try {
   if (global) {
