84e51915a8ceaa7afa69a9fed38a8f4b458690cf
Mac-thartcher <brankiki19@gmail.com>
2025-11-30 15:31:42 +0200

    week8 day2

---
+++ user/week08/exercises/day04/script.js
@@ -0,0 +1,18 @@
+const userInputA =42;
+const userInputB = "43";
+
+const userNumberInputB = Number(userInputB);
+Console.console.log(typeof userInputB,userNumberInputB);
+console.log()
+
+
+
+
+console.log("TEST OUR KNOWLEDGE #2:",
+    false === 1,
+);
+const opA = false;
+const opB = 1;
+const booleanOpB = Boolean(opB);
+console.log(typeof opB,booleanOpB);
+console.log()
