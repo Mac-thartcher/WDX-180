602744c374f3272b3d1aa64db31d2eb72f27ff0f
Mac-thartcher <brankiki19@gmail.com>
2026-01-10 21:26:19 +0200

    completed consuming-promises

---
+++ user/week13/exercises/day01/02-consuming-promises/answers.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * EXERCISE 1
+ * 
+ * @param {Promise} promise
+ * @param {thunk} action
+ * 
+ */
+function waitForPromise(promise, action){
+  /* IMPLEMENT ME */
+  promise.then(() =>{
+    action()
+  })
+}
+/**
+ * 
+ * EXERCISE 2
+ * 
+ * @param {Promise} promise 
+ * @param {consumer} consumer 
+ * @param {handler} handler 
+ */
+function consumePromise(promise, consumer, handler){
+  /* IMPLEMENT ME! */
+  promise .then ((data) => {
+    consumer(data);
+  })
+  .catch((error) => {
+    handler(error);
+  })
+}
+
+/**
+ * @callback thunk
+ * @returns {void}
+ */
+module.exports = {
+  waitForPromise,
+  consumePromise,
+};
