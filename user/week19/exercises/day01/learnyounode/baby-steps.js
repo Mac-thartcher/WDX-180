4a9b14dd0d852cb6aeaf8aad6585f3a684ab793d
Mac-thartcher <brankiki19@gmail.com>
2026-02-23 18:15:30 +0200

    baby-steps.js

---
+++ user/week19/exercises/day01/learnyounode/baby-steps.js
@@ -0,0 +1,14 @@
+// let sum = 0;
+
+// for (let i = 2; i < process.argv.length; i++) {
+//   sum += Number(process.argv[i]);
+// }
+// console.log(sum);
+
+ let result = 0
+
+    for (let i = 2; i < process.argv.length; i++) {
+      result += Number(process.argv[i])
+    }
+
+    console.log(result)
